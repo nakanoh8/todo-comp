0b9e257df254a02be75ab1b18394abd81a44b137
nakanoh <murry.xxx.7@gmail.com>
2020-06-25 23:31:04 +0900

    todo.vueを追加

---
+++ components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar class="mb-4" toggleable="md" type="dark" variant="info">
+    <router-link to="/">
+      <b-navbar-brand>BookShelf</b-navbar-brand>
+    </router-link>
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav_collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{ path: '/todo' }">今日のTodo</b-nav-item>
+        <!-- <b-nav-item :to="{ path: '/add' }">本を追加する</b-nav-item> -->
+        <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      q: '',
+    }
+  },
+
+  methods: {
+    searchBook(e) {
+      e.preventDefault()
+      this.$router.push({ path: '/search', query: { q: this.q } })
+    },
+  },
+}
+</script>
